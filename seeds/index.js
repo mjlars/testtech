8b3975474695dc196b09d2f095757d0296c01ea9
Matthew Larson <34934109+mjlars@users.noreply.github.com>
2022-04-09 14:25:26 -0600

    Add files via upload

---
+++ seeds/index.js
@@ -0,0 +1,20 @@
+const seedUsers = require('./user-seeds');
+const seedProfiles = require('./post-seeds');
+
+
+const sequelize = require('../config/connection');
+const seedPosts = require('./post-seeds');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('--------------');
+    await seedUsers();
+    console.log('--------------');
+
+    await seedPosts();
+    console.log('--------------');
+  
+    process.exit(0);
+  };
+  
+seedAll();
